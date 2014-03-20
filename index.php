37a0c3bb8ecea26439415dcd31a205e45f776093
maxiaquino0 <maxiaquino0@gmail.com>
2014-03-20 19:36:19 -0300

    Actualizando mi index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 </head>
 <body>
 	<h1>Sistema ProyUTN</h1>
+	<h2>Un ABM de productos</h2>
 	<a href='listar_productos.php'>Listar Productos</a>
 	<br>
 	<a href='agregar_producto.php'>Agregar Producto</a>
